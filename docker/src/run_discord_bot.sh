e8668cc81954ec970c9c2dcfe3c9f3d4d0c50922
SirRu24 <mna.bro6@gmail.com>
2025-05-27 00:16:42 +0800

    v1.2.0.0
    * Dockerfile to pull the latest commit of the repository
    * Added THREAD_ID into the environment
    * Updated run_discord_bot.sh to build and run based on the arch

--- docker/src/run_discord_bot.sh
+++ docker/src/run_discord_bot.sh
@@ -1,2 +1,28 @@
+#!/bin/bash
+
 source .env
-dotnet run RuTakingTooLong.csproj --runtime linux-arm64
+
+ARCH=$(uname -m)
+
+case "$ARCH" in
+    x86_64)
+        RID="linux-x64"
+        ;;
+    aarch64)
+        RID="linux-arm64"
+        ;;
+    armv7l)
+        RID="linux-arm"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
+echo "Using RID: $RID"
+
+dotnet publish -c Release -r $RID --self-contained true -p:PublishSingleFile=true
+
+chmod u+x bin/Release/net8.0/$RID/publish/RuTakingTooLong
+./bin/Release/net8.0/$RID/publish/RuTakingTooLong
