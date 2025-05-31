684c02f7a62db404b8aed9345f006253462986f9
SirRu24 <mna.bro6@gmail.com>
2025-05-31 16:28:49 +0800

    v1.3.0.0
    * Compile sources when setting up docker image
    * Move compiled app to working directory when setting up docker image
    * Modified docker script to set the run_discord_bot.sh as the entrypoint
    * Modified docker script to send SIGINT to docker container so the app can close gracefully

--- docker/bot_start.sh
+++ docker/bot_start.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
+
 docker buildx build --file Dockerfile --tag discord-bot:common --load .
-docker run -d --name discord-bot-container discord-bot:common tail -f /dev/null
-docker exec -d discord-bot-container bash -c "cd ~/discord-bot/HoloSimpCart-Net && ./run_discord_bot.sh"
+docker run -d --name discord-bot-container --stop-signal SIGINT discord-bot:common
--- docker/bot_stop.sh
+++ docker/bot_stop.sh
@@ -1,3 +1,5 @@
-docker stop discord-bot-container
+#!/bin/bash
+
+docker stop --signal=SIGINT discord-bot-container
 docker rm discord-bot-container
 docker rmi discord-bot:common
