7e339fb135e01e7f6a6c4f26ace124341ad58513
SirRu24 <mna.bro6@gmail.com>
2025-06-30 22:03:19 +0800

    v1.7.0.0
    * Add MMOR.Utils

--- scripts/setup/bot_cart_net_setup.sh
+++ scripts/setup/bot_cart_net_setup.sh
@@ -5,6 +5,9 @@ HOLOSIMP_IDSG_REMOTE=https://github.com/HoloSimpID-SG
 DISCORD_BOT_NET_REPO=${HOLOSIMP_IDSG_REMOTE}/HoloSimpCart-Net.git
 DISCORD_BOT_NET_REF=main
 
+MMORUTILS_REPO=https://github.com/mcahyadit/MMOR.Utils.git
+MMORUTILS_REF=main
+
 # Git clone HoloSimpCart-Net inside docker/src
 cd docker/src
 rm -rf HoloSimpCart-Net
@@ -12,5 +15,13 @@ git clone ${DISCORD_BOT_NET_REPO} HoloSimpCart-Net --branch $DISCORD_BOT_NET_REF
 cd HoloSimpCart-Net
 git checkout $DISCORD_BOT_NET_REF
 git pull origin $DISCORD_BOT_NET_REF
+
+# Git clone MMOR.Utils inside docker/src
+cd ../
+rm -rf MMOR.Utils
+git clone ${MMORUTILS_REPO} MMOR.Utils --branch $MMORUTILS_REF
+cd MMOR.Utils
+git checkout $MMORUTILS_REF
+git pull origin $MMORUTILS_REF
 
 cd ../../
